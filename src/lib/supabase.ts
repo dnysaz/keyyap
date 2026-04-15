4db40296a9a8b0881333f0eebf894d45da5892fa
dnysaz <danayasa2@gmail.com>
2026-04-15 15:02:50 +0800

    first commit

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -8,10 +8,5 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: true
-  },
-  realtime: {
-    params: {
-      eventsPerSecond: 10,
-    },
-  },
+  }
 })

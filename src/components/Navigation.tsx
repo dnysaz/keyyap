65ad2fa4961ce53991c571356d2eb8b3f310a02b
dnysaz <danayasa2@gmail.com>
2026-04-19 12:11:15 +0800

    Tambah fitur PWA & App Icons

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { Home, Search, Pencil, Bell, User } from 'lucide-react'
+import { Globe, Search, PencilLine, Bell, User } from 'lucide-react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import Avatar from './Avatar'
 import { useAuthStore } from '@/stores/authStore'
 import { useNotificationStore } from '@/stores/notificationStore'
 
@@ -15,15 +16,15 @@ export default function Navigation() {
 
   return (
     <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 z-50 pb-safe">
-      <div className="flex items-center justify-around h-16 max-w-lg mx-auto">
+      <div className="flex items-center justify-around h-[76px] pb-4 max-w-lg mx-auto">
         <Link href="/" className={isActive('/') ? 'text-gray-900' : 'text-gray-500'}>
-          <Home className="w-7 h-7" />
+          <Globe className="w-7 h-7" />
         </Link>
         <Link href="/search" className={isActive('/search') ? 'text-gray-900' : 'text-gray-500'}>
           <Search className="w-7 h-7" />
         </Link>
         <Link href="/create">
-          <Pencil className="w-7 h-7 text-gray-500" />
+          <PencilLine className="w-7 h-7 text-gray-500" />
         </Link>
         <Link href="/notifications" className={`relative ${isActive('/notifications') ? 'text-gray-900' : 'text-gray-500'}`}>
           <Bell className="w-7 h-7" />
@@ -34,7 +35,13 @@ export default function Navigation() {
           )}
         </Link>
         <Link href={user ? `/u/${profile?.username || 'me'}` : '/login'} className={isActive('/login') ? 'text-gray-900 font-bold' : 'text-gray-500'}>
-          <User className="w-7 h-7" />
+          {user ? (
+            <div className={`rounded-full border-[1.5px] p-[1px] ${isActive(`/u/${profile?.username}`) ? 'border-gray-900 text-gray-900' : 'border-transparent text-gray-500'}`}>
+              <Avatar url={profile?.avatar_url || undefined} username={profile?.username || undefined} size="xs" className="w-[26px] h-[26px] text-xs" />
+            </div>
+          ) : (
+            <User className="w-7 h-7" />
+          )}
         </Link>
       </div>
     </nav>
